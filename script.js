0c4e623daccadb67ac36f805ff9820bf63859165
saito0120 <132254371+saito0120@users.noreply.github.com>
2026-03-23 09:41:54 +0000

    topボタン追加

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 //topボタン常に出す処理
 const btn = document.getElementById('back-to-top');
 
+// スクロールでボタン表示
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {  // 200px以上スクロールしたら表示
+        btn.style.display = 'block';
+    } else {
+        btn.style.display = 'none';
+    }
+});
+
+// クリックでトップへスムーズスクロール
 btn.addEventListener('click', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
